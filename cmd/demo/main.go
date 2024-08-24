d98235d89b83fed395a112c0aac7fd8984acac89
So Koide <sokoide@gmail.cojm>
2024-08-24 14:07:42 +0900

    refactor: add claude conn counter

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,35 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"sync/atomic"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/sokoide/ws-ai/pkg/claude"
 )
 
-func main() {
-	log.Info("demo")
+var claudeConns int32
 
+func startUser(id string, firstQ string) {
 	cin := make(chan claude.Request, 1)
 	cout := make(chan claude.Response, 1)
 
-	go claude.StartConversation("foo@example.com", cin, cout)
+	go claude.StartConversation(id, cin, cout)
+	atomic.AddInt32(&claudeConns, 1)
 
-	for _, q := range []string{"What's the biggest city in Japan?", "Area"} {
+	for _, q := range []string{firstQ, "Summarize it in 1 sentence"} {
 		cin <- claude.Request{Prompt: q}
 		res := <-cout
 
 		if res.Succeeded {
-			log.Infof("ans: %s\n", res.Text)
+			log.Infof("[%s] ans: %s\n", id, res.Text)
 		} else {
-			log.Infof("error: %s\n", res.Text)
+			log.Infof("[%s] error: %s\n", id, res.Text)
 		}
 	}
-
 	close(cin)
+	atomic.AddInt32(&claudeConns, -1)
+}
 
-	time.Sleep(time.Second)
+func main() {
+	log.Info("demo")
 
-	fmt.Println("done")
+	go startUser("foo@localhost", "make a story of a dragon in 100 words")
+	go startUser("bar@localhost", "what's the biggest city in India?")
+	go startUser("baz@localhost", "what's the largest animal on earth?")
+
+	for i := 0; i < 15; i++ {
+		log.Infof("%d conns\n", atomic.LoadInt32(&claudeConns))
+		time.Sleep(time.Second)
+	}
+
+	log.Info("done")
 }
